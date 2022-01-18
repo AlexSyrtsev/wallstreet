abbcf1abe6f258cea4fcf7b94963376ffddc5add
Алексей Сырцев <79574158+AlexSyrtsev@users.noreply.github.com>
2022-01-18 16:09:15 +0300

    Wall Str. Co
    Wall Str. Co

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(window).resize(function (event) {
+	adaptive_function();
+});
+function adaptive_header(w, h) {
+	var headerMenu = $('.header-menu');
+	var headerLang = $('.header-top-lang');
+	if (w < 767) {
+		if (!headerLang.hasClass('done')) {
+			headerLang.addClass('done').appendTo(headerMenu);
+		}
+	} else {
+		if (headerLang.hasClass('done')) {
+			headerLang.removeClass('done').prependTo($('.header-top'));
+		}
+	}
+	if (w < 767) {
+		if (!$('.header-bottom-menu').hasClass('done')) {
+			$('.header-bottom-menu').addClass('done').appendTo(headerMenu);
+		}
+	} else {
+		$.each($('.header-bottom-menu'), function (index, val) {
+			if ($(this).hasClass('header-bottom-menu--right')) {
+				if ($(this).hasClass('done')) {
+					$(this).removeClass('done').prependTo($('.header-bottom__column').eq(2));
+				}
+			} else {
+				if ($(this).hasClass('done')) {
+					$(this).removeClass('done').prependTo($('.header-bottom__column').eq(0));
+				}
+			}
+		});
+	}
+}
+
+function adaptive_function() {
+	var w = $(window).outerWidth();
+	var h = $(window).outerHeight();
+	adaptive_header(w, h);
+}
+adaptive_function();
+
+
+$('.menu-header__icon').click(function (event) {
+	$(this).toggleClass('active');
+	$('.menu-header__menu').toggleClass('active');
+	if ($(this).hasClass('active')) {
+		$('body').data('scroll', $(window).scrollTop());
+	}
+	$('body').toggleClass('lock');
+	if (!$(this).hasClass('active')) {
+		$('body, html').scrollTop(parseInt($('body').data('scroll')));
+	}
+});
+
+
+
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg"); for (var i = 0; i < ibg.length; i++) { if (ibg[i].querySelector('img')) { ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')'; } }
+}
+ibg();
